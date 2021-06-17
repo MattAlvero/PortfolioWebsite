f5c77431c9712f41316f06e3a83021aceae48d41
MattAlvero <matthewadrianoalvero@gmail.com>
2021-06-17 10:25:41 -0500

    Added JS Script for navbar and submiting form

--- js/script.js
+++ js/script.js
@@ -1,15 +1,45 @@
-const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
-const list = new MDCList(document.querySelector('.mdc-dialog .mdc-list'))
-const button = new MDCRipple(document.querySelector('.mdc-button'));
+// Scrolling animation to about from navbar
+document.getElementById("toAbout").addEventListener("click", function(){
+    document.getElementById("about").scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+  // Scrolling animation to experience from navbar
+  document.getElementById("toExp").addEventListener("click", function(){
+    document.getElementById("experience").scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+  // Scrolling animation to portfolio from navbar
+  document.getElementById("toPortfolio").addEventListener("click", function(){
+    document.getElementById("portfolio").scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+  // Scrolling animation to links from navbar
+  document.getElementById("toLinks").addEventListener("click", function(){
+    document.getElementById("links").scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+  // Scrolling animaiton to contact from navbar
+  document.getElementById("toContact").addEventListener("click", function(){
+    document.getElementById("contact").scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
 
-dialog.list('MDCDialog:opened', () => {
-    list.layout();
-});
-
-button.addEventListener('click', (event) => {
-    if (dialog.open)
-        dialog.open = false;
-    else {
-        dialog.open = true;
-    }
-});
+  // Close dropdown navbar after clicking on one of the items
+  // const navLinks = document.querySelectorAll('.nav-item');
+  // const menuToggle = document.getElementById('navItems');
+  // const bsCollapse = new bootstrap.Collapse(menuToggle);
+  // navLinks.forEach((l) => {
+  //   l.addEventListener('click', () => {
+  //     bsCollapse.toggle()
+  //   })
+  // });
+  
+  // Alert when submitting form
+  document.getElementById("MessageSubmit").addEventListener('click', () => {
+    alert("Form submitted, thank you!");
+  });
